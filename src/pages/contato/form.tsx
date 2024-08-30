1b79b4d761615356f9bc0c2194e4c40b9d7822e8
Janilson Varela <janilsonjvs@gmail.com>
2024-08-30 19:50:32 -0300

    feat: formulario de contato

---
+++ src/pages/contato/form.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ContatoForm: React.FC = () => {
+  return (
+    <div>
+      <h1>ContatoForm</h1>
+        <Link to="/contatos">Voltar</Link>
+
+    </div>
+  );
+};
+
+export default ContatoForm;
