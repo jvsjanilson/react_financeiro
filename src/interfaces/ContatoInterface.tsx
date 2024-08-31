5e7dfa7ecc60f8e7a677dafcc5c1465b8b13da59
Janilson Varela <janilsonjvs@gmail.com>
2024-08-31 09:40:08 -0300

    feat: interface contato

---
+++ src/interfaces/ContatoInterface.tsx
@@ -0,0 +1,16 @@
+export interface IContato {
+    id: number;
+    nome: string;
+    cpf_cnpj: string;
+    endereco: string;
+    numero: string;
+    cep: string;
+    complemento: string;    
+    bairro: string;
+    cidade: string;
+    estado: string;
+    email: string;
+    telefone: string;
+    celular: string;
+    ativo: boolean;
+}
