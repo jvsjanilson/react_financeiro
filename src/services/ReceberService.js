4a58b011434de73b872c86b9a87375026e819e4a
Janilson Varela <janilsonjvs@gmail.com>
2024-09-01 11:33:32 -0300

    feat: implementado baixa e estorno no service

--- src/services/ReceberService.js
+++ src/services/ReceberService.js
@@ -2,41 +2,50 @@ import api from "../api/api";
 
 class ReceberService {
         
-        async getAll(search, page) {
-            let query = '';
-            if (search?.length > 0) {
-                query = `?search=${search}`;
-                query += page ? `&${page}` : '';
-            } else  {
-                query = page ? `?${page}` : '';
+    async getAll(search, page) {
+        let query = '';
+        if (search?.length > 0) {
+            query = `?search=${search}`;
+            query += page ? `&${page}` : '';
+        } else  {
+            query = page ? `?${page}` : '';
+        }
+    
+        return api.get(`/recebers/${query}`).then((response) => {
+                console.log(response.data);
+                return response.data;
             }
-        
-            return api.get(`/recebers/${query}`).then((response) => {
-                    console.log(response.data);
-                    return response.data;
-                }
-            );
-        
-        }
-        
-        async get(id) {
-            return api.get(`/recebers/${id}/`).then((response) => {
-                    return response.data
-                }
-            );
-        }
-        
-        async create(body) {
-            return api.post("/recebers/", body);
-        }
-        
-        async update(id, body) {
-            return api.put(`/recebers/${id}/`, body);
-        }
-        
-        async delete(id) {
-            return api.delete(`/recebers/${id}/`);
-        }
+        );
+    
+    }
+    
+    async get(id) {
+        return api.get(`/recebers/${id}/`).then((response) => {
+                return response.data
+            }
+        );
+    }
+    
+    async create(body) {
+        return api.post("/recebers/", body);
+    }
+    
+    async update(id, body) {
+        return api.put(`/recebers/${id}/`, body);
+    }
+    
+    async delete(id) {
+        return api.delete(`/recebers/${id}/`);
+    }
+
+    async baixar(id, body) {
+        return api.post(`/recebers/${id}/baixar-receber/`, body);
+    }
+
+    async estornar(id, body) {
+        return api.post(`/recebers/${id}/estornar-receber/`, body);
+    }
+
 }
 
 const receberService = new ReceberService();
